67b79e3d3297205da1cb0010b576044bdcc49081
Shir Balmas <shirb27@gmail.com>
2018-08-14 13:40:50 +0300

    shir changes 14.8

--- myfiles/PHP/insertRegistration.php
+++ myfiles/PHP/insertRegistration.php
@@ -13,6 +13,20 @@
 	$zipCode=$_POST['zipCode'];
 
 
+//checkbox
+$tripArr=$_POST['tripType']; //save in array variable
+$userTripType=implode(",",$tripArr); //add comma ,
+
+$ameniArr=$_POST['amenities']; //save in array variable
+$userAmeni=implode(",",$ameniArr); //add comma ,
+
+$reqArr=$_POST['requests']; //save in array variable
+$userReq=implode(",",$reqArr); //add comma ,
+
+$accessArr=$_POST['accessibilityNeeds']; //save in array variable
+$userAcces=implode(",",$accessArr); //add comma ,
+
+
 $servername="localhost";
 $username="root";
 $password="";
@@ -25,8 +39,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "INSERT INTO users (userID, Fname, Lname, email, password, phone, country, city, street, aptNum, zipCode)
-VALUES ('$userID', '$Fname', '$Lname' , '$email', '$pass', '$phone', '$country', '$city', '$street', '$aptNum', '$zipCode')";
+$sql = "INSERT INTO users (userID, Fname, Lname, email, password, phone, country, city, street, aptNum, zipCode, tripType, amenities, requests, accessibilityNeeds)
+VALUES ('$userID', '$Fname', '$Lname' , '$email', '$pass', '$phone', '$country', '$city', '$street', '$aptNum', '$zipCode', '$userTripType', '$userAmeni', '$userReq' , '$userAcces')";
 
 if ($conn->query($sql) === TRUE) {
     header("Location:..\sendPage.html");
